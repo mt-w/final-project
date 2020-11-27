9f1e92a46007e8f93ae0ac15c47f386a615ac7b3
Daria Lukina <71461964+mt-w@users.noreply.github.com>
2020-11-27 22:27:53 +0300

    Update page.js

--- final_task/src/redux/reducers/page.js
+++ final_task/src/redux/reducers/page.js
@@ -1,45 +1,44 @@
-import {
-  INCREMENT_PAGE_NUMBER,
-  INCREMENT_CAUGHT_PAGE_NUMBER,
-  DECREMENT_PAGE_NUMBER,
-  DECREMENT_CAUGHT_PAGE_NUMBER,
-} from "../actions/page";
+import { INCREMENT_PAGE_NUMBER, DECREMENT_PAGE_NUMBER } from "../actions/page";
 
 const initState = {
   page: 1,
-  caughtPage: 1,
-  isFirstPage: false,
+  isFirstPage: true,
   isLastPage: false,
-  isPageLoaded: true
+  isPageLoaded: true,
+};
+const getIncPageStatus = (pageNum, lastPage) => {
+    pageNum === 846 ? (lastPage = true) : (lastPage = false);
+    return lastPage;
+}; //846 изменить на вычисляемый относительно all items length параметр
+
+const getDecPageStatus = (pageNum, firstPage) => {
+  pageNum < 2 ? (firstPage = true) : (firstPage = false);
+  return firstPage;
 };
 
- const page = (state = initState, action) => {
+const page = (state = initState, action) => {
   switch (action.type) {
     case INCREMENT_PAGE_NUMBER:
+      const newIncPage = state.page + 1;
+      console.log("INC Page" + newIncPage);
       return {
         ...state,
-        page: state.page + 1,
-        isPageLoaded: true
-      };
-    case INCREMENT_CAUGHT_PAGE_NUMBER:
-      return {
-        ...state,
-        caughtPage: state.caughtPage + 1,
-          isPageLoaded: true
+        page: newIncPage,
+        isFirstPage: getDecPageStatus(newIncPage),
+        isLastPage: getIncPageStatus(newIncPage),
+        isPageLoaded: true,
       };
 
     case DECREMENT_PAGE_NUMBER:
+      const newPage = state.page - 1;
       return {
         ...state,
-        page: state.page - 1,
-          isPageLoaded: true
+        page: newPage,
+          isFirstPage: getDecPageStatus(newPage),
+          isLastPage:getIncPageStatus(newPage),
+        isPageLoaded: true,
       };
-    case DECREMENT_CAUGHT_PAGE_NUMBER:
-      return {
-        ...state,
-        caughtPage: state.caughtPage - 1,
-          isPageLoaded: true
-      };
+
     case "SET_PAGE_LOADED":
       return {
         ...state,
